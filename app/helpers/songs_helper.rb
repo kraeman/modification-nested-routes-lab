0730e2457ae0c0658c0e8ccc20efbf362c946596
Krae <kraewind@gmail.com>
2021-03-22 14:27:53 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -6,13 +6,4 @@ module SongsHelper
           hidden_field_tag "song[artist_id]", song.artist_id
         end
     end
-
-    def artist_select(song)
-    
-      if song.artist
-        link_to(song.artist.name, artist_path(song.artist))
-      else
-        link_to("Add Artist", edit_song_path(song))
-      end
-    end
 end
