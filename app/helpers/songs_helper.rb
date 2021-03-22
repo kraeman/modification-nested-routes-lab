fc9092704adf04051a9434c8685902e444de6071
Krae <kraewind@gmail.com>
2021-03-22 14:18:36 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,18 @@
 module SongsHelper
+    def artist_id_field(song)
+        if song.artist.nil?
+          select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+        else
+          hidden_field_tag "song[artist_id]", song.artist_id
+        end
+    end
 
+    def artist_select(song)
+    
+      if song.artist
+        link_to(song.artist.name, artist_path(song.artist))
+      else
+        link_to("Add Artist", edit_song_path(song))
+      end
+    end
 end
